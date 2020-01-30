5cc8cc8c3a94ed96b2217b2f43a527fd7483c2c7
Nat Dudley <59619648+natdudley-EDA@users.noreply.github.com>
2020-01-31 11:59:00 +1300

    get route working to dynamically show animation

--- client/components/Animation.jsx
+++ client/components/Animation.jsx
@@ -3,13 +3,17 @@ import { Route, Link } from 'react-router-dom'
 
 const Animation = props => {
   const id = props.match.params.id
-  const name = props.match.params.name
+  let name = props.match.url
+  const index = (name.lastIndexOf('/') + 1)
+  name = name.slice(index)
+  let classes = name + ' character'
+
   return (
     <>
 
-    <h2>Display ({id}'s) animation ({name})</h2>
-    <img src={`/${id}.png`} alt="Image of character"/>
-    {<img className="glasses" src="glasses.png"/>}
+    <h2>Display {id}'s animation {name}</h2>
+    <img src={`/${id}.png`} alt="Image of character" className={classes}/>
+    {name === 'dealWithIt' && <img className="glasses" src="glasses.png"/>}
     <Link to={`/showOptions/${id}`}>GO BACK</Link><br></br>
     <Link to="/">HOME</Link>
     </>
