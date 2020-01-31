864871ef55437cafc93408b962f1c21a867486df
Hamish Stobo <51815749+HHSNZ@users.noreply.github.com>
2020-01-31 14:16:52 +1300

    add files for commit

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -4,6 +4,9 @@ import { Link, Route } from 'react-router-dom'
 const Home = () => {
   return (
     <>
+    <audio controls>
+      <source src="maintheme.mp3" type="audio/mp3">
+    </audio>
     <Link className="gridlink" to="/chargrid">
       <div className="home-body">
         <div className="fade"></div>
@@ -19,7 +22,7 @@ const Home = () => {
             <p>It is a period of SQL war. Rebel coders, striking from a hidden base, have won their first victory against the evil Galactic Empire.</p>
             <p>Rebel coders have managed to harness the force to transform the franchises most beloved characters.</p>
             <p>Defeat the Empire’s sinister agents by selecting your favorite character and applying a range of powerful transformations.</p>
-            <p>Click Below to Begin.</p>
+            <p>Click to Begin.</p>
           </div>
         </section>
       </div>
